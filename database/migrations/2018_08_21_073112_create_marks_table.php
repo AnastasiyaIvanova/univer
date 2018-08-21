1a5dbff30bbcff1644ea574e5bb0bb8e25f1a559
Nastya Ivanova <nastasya_ivanova_2015@mail.ru>
2018-08-21 14:04:15 +0400

    first commit

---
+++ database/migrations/2018_08_21_073112_create_marks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('student_id')->unsigned()->index();
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->integer('subject_id')->unsigned()->index();
+            $table->foreign('subject_id')->references('id')->on('subjects');
+            $table->date('date');
+            $table->integer('mark');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marks');
+    }
+}
